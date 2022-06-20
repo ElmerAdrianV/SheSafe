b6a86a2e06cae738e3e1dddf1be75049b5798e3a
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-20 15:45:27 -0700

    Fix a typo

--- app/src/main/java/com/elmeradrianv/shesafe/LaunchActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/LaunchActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class LaunchActivity extends AppCompatActivity {
-    rivate static final int SPLASH_SCREEN_DURATION_MS = 2000;
+    private static final int SPLASH_SCREEN_DURATION_MS = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

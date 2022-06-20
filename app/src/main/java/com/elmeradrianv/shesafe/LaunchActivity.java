451be8b4bc199687e847d902c3fa9c5d5940d317
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-20 14:27:54 -0700

    Add feature at Launch Activity

--- app/src/main/java/com/elmeradrianv/shesafe/LaunchActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/LaunchActivity.java
@@ -2,7 +2,9 @@ package com.elmeradrianv.shesafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(LaunchActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        },2000);
     }
 }

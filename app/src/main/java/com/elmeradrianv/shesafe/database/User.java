09529d5bbc280de5b47884dfb0ea0b5a21f66491
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-22 14:48:18 -0700

    Add a save method in User class with photo

--- app/src/main/java/com/elmeradrianv/shesafe/database/User.java
+++ app/src/main/java/com/elmeradrianv/shesafe/database/User.java
@@ -1,6 +1,7 @@
 package com.elmeradrianv.shesafe.database;
 
 import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -18,10 +19,10 @@ public class User extends ParseUser {
     public static final String PERSONAL_DESCRIPTION_KEY = "personalDescription";
     public static final String PROFILE_PHOTO_KEY = "profilePhoto";
     public static final String FIRST_NAME_KEY = "firstName";
-    public static final String LAST_NAME_KEY="lastName";
+    public static final String LAST_NAME_KEY = "lastName";
     public static final String TAG = User.class.getSimpleName();
 
-    private void saveWithoutImage(Context context, String username, String firstName, String lastName, String email, String personalDescription, String password) {
+    public void saveWithoutImage(Context context, String username, String firstName, String lastName, String email, String personalDescription, String password) {
         ParseUser user = new ParseUser();
         user.setUsername(username);
         user.put(User.FIRST_NAME_KEY, firstName);
@@ -36,6 +37,33 @@ public class User extends ParseUser {
                 return;
             }
             Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public void saveWithImage(Context context, File profilePhoto, String username, String firstName, String lastName, String email, String personalDescription, String password) {
+        ParseUser user = new ParseUser();
+        ParseFile file = new ParseFile(profilePhoto);
+        file.saveInBackground((SaveCallback) e -> {
+            // If successful add file to user and signUpInBackground
+            if (e != null)
+                Log.e(TAG, "done save profile image: ", e);
+            else {
+                user.put(User.PROFILE_PHOTO_KEY, file);
+                user.setUsername(username);
+                user.put(User.FIRST_NAME_KEY, firstName);
+                user.put(User.LAST_NAME_KEY, lastName);
+                user.setEmail(email);
+                user.put(User.PERSONAL_DESCRIPTION_KEY, personalDescription);
+                user.setPassword(password);
+                user.signUpInBackground(e2 -> {
+                    if (e != null) {
+                        Toast.makeText(context, "Couldn't sign up", Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "signupNewUser: Signup error", e2);
+                        return;
+                    }
+                    Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show();
+                });
+            }
         });
     }
 

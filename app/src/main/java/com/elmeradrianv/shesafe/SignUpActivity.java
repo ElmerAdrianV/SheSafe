5eac7ecf838234ef1209250c08befcbdb7d7ba96
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-22 14:37:54 -0700

    Save images picking from the gallery

--- app/src/main/java/com/elmeradrianv/shesafe/SignUpActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.elmeradrianv.shesafe;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +17,19 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.elmeradrianv.shesafe.database.User;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
     private static final String TAG = SignUpActivity.class.getSimpleName();
-
+    // PICK_PHOTO_CODE is a constant integer
+    public final static int PICK_PHOTO_CODE = 1046;
+    private static boolean PERSONALIZED_PHOTO_PICKED = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +46,17 @@ public class SignUpActivity extends AppCompatActivity {
         EditText etPassword = findViewById(R.id.etPassword);
         EditText etPasswordConfirm = findViewById(R.id.etPasswordConfirm);
         Button btnSignUp = findViewById(R.id.btnSignUp);
+        Button btnAddProfilePhoto = findViewById(R.id.btnAddProfilePhoto);
+
+
         setImage("", ivProfilePhoto);
         setupBtnSignUp(btnSignUp, etUsername, etFirstName, etLastName, etEmail, etPersonalDescription, etPassword, etPasswordConfirm);
+        setBtnAddProfilePhoto(btnAddProfilePhoto);
+
 
 
     }
+
 
     private void setupBtnSignUp(Button btnSignUp, EditText etUsername, EditText etFirstName, EditText etLastName, EditText etEmail, EditText etPersonalDescription, EditText etPassword, EditText etPasswordConfirm) {
         btnSignUp.setOnClickListener(v -> {
@@ -63,14 +79,46 @@ public class SignUpActivity extends AppCompatActivity {
 
         });
     }
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+
+    }
 
     private void signupNewUser(String username, String firstName, String lastName, String email, String personalDescription, String password) {
         ParseUser user = new ParseUser();
+
+        if(PERSONALIZED_PHOTO_PICKED){
+            ImageView ivProfilePhoto = findViewById(R.id.ivProfilePhoto);
+            File profilePhoto = getPhotoFileUri(ivProfilePhoto.getTag().toString());
+            ParseFile file = new ParseFile(profilePhoto);
+
+            file.saveInBackground((SaveCallback) e -> {
+                // If successful add file to user and signUpInBackground
+                if(e != null)
+                    Log.e(TAG, "done save profile image: ",e);
+                else
+                    user.put(User.PROFILE_PHOTO_KEY,file);
+            });
+
+        }
         user.setUsername(username);
-        user.put(User.FIRST_NAME_KEY,firstName);
-        user.put(User.LAST_NAME_KEY,lastName);
+        user.put(User.FIRST_NAME_KEY, firstName);
+        user.put(User.LAST_NAME_KEY, lastName);
         user.setEmail(email);
-        user.put(User.PERSONAL_DESCRIPTION_KEY,personalDescription);
+        user.put(User.PERSONAL_DESCRIPTION_KEY, personalDescription);
         user.setPassword(password);
         user.signUpInBackground(e -> {
             if (e != null) {
@@ -83,16 +131,51 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
-    private void setImage(String url, ImageView imageView) {
-        int radiusIP = 100;
-        Glide.with(this).load(url)
+    private void setBtnAddProfilePhoto(Button btnAddProfilePhoto) {
+        btnAddProfilePhoto.setOnClickListener(v -> {
+            onPickPhoto();
+        });
+    }
+
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto() {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+            PERSONALIZED_PHOTO_PICKED=true;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+            // Load the selected image into a preview
+            ImageView ivProfilePhoto =  findViewById(R.id.ivProfilePhoto);
+            ivProfilePhoto.setTag(photoUri.toString());
+            setImage(photoUri.toString(),ivProfilePhoto);
+        }
+    }
+
+    private void setImage(String uri, ImageView imageView) {
+        int radius = 100000;
+        Glide.with(this).load(uri)
                 .apply(new RequestOptions()
                         .centerCrop()
                         .placeholder(R.drawable.ssplaceholder)
-                        .transform(new RoundedCorners(radiusIP))
+                        .transform(new RoundedCorners(radius))
                 )
                 .into(imageView);
     }
+
     private void goMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);

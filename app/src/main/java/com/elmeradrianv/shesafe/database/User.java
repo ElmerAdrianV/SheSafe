68835250b5ff5432db84a35cc9bbaf97d500a19e
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-22 11:11:49 -0700

    Update User model

--- app/src/main/java/com/elmeradrianv/shesafe/database/User.java
+++ app/src/main/java/com/elmeradrianv/shesafe/database/User.java
@@ -8,6 +8,8 @@ import com.parse.ParseUser;
 public class User extends ParseUser {
     public static final String PERSONAL_DESCRIPTION_KEY = "personalDescription";
     public static final String PROFILE_PHOTO_KEY = "profilePhoto";
+    public static final String FIRST_NAME_KEY = "firstName";
+    public static final String LAST_NAME_KEY="lastName";
 
     public String getPersonalDescription() {
         return getString(PERSONAL_DESCRIPTION_KEY);
@@ -16,7 +18,6 @@ public class User extends ParseUser {
     void setPersonalDescription(String personalDescription) {
         put(PERSONAL_DESCRIPTION_KEY, personalDescription);
     }
-
     public ParseFile getProfilePhoto() {
         return getParseFile(PROFILE_PHOTO_KEY);
     }
@@ -24,5 +25,9 @@ public class User extends ParseUser {
     void setPersonalDescriptionKey(ParseFile photo) {
         put(PROFILE_PHOTO_KEY, photo);
     }
+    public String getFirstName(){return getString(FIRST_NAME_KEY);}
+    public void setFirstName(String firstName){put(FIRST_NAME_KEY,firstName);}
 
+    public String getLastName(){return getString(LAST_NAME_KEY);}
+    public void setLastName(String lastName){put(LAST_NAME_KEY,lastName);}
 }

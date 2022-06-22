ef5fcade06df5ecf419d82e881a9ad0331dea366
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-22 11:37:16 -0700

    Add check feature in the Signup

--- app/src/main/java/com/elmeradrianv/shesafe/LoginActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
 
         EditText etUsername = findViewById(R.id.etUsername);
         EditText etPassword = findViewById(R.id.etLastName);
-        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnLogin = findViewById(R.id.btnAddProfilePhoto);
         Button btnSignUp = findViewById(R.id.btnSignUp);
         //To hide the action bar in appcompact activity
         getSupportActionBar().hide();
--- app/src/main/java/com/elmeradrianv/shesafe/SignUpActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/SignUpActivity.java
@@ -1,11 +1,12 @@
 package com.elmeradrianv.shesafe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
@@ -19,22 +20,49 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         //To hide the action bar in appcompact activity
         getSupportActionBar().hide();
+
+        ImageView ivProfilePhoto = findViewById(R.id.ivProfilePhoto);
         EditText etUsername = findViewById(R.id.etUsername);
         EditText etFirstName = findViewById(R.id.etFirstName);
         EditText etLastName = findViewById(R.id.etLastName);
         EditText etEmail = findViewById(R.id.etEmail);
         EditText etPersonalDescription = findViewById(R.id.etPersonalDescription);
         EditText etPassword = findViewById(R.id.etPassword);
-        EditText etPassConfirm = findViewById(R.id.etPasswordConfirm);
-        ImageView ivProfilePhoto = findViewById(R.id.ivProfilePhoto);
-
-        setImage("",ivProfilePhoto);
-
-
+        EditText etPasswordConfirm = findViewById(R.id.etPasswordConfirm);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+        setImage("", ivProfilePhoto);
+        setupBtnSignUp(btnSignUp, etUsername, etFirstName, etLastName, etEmail, etPersonalDescription, etPassword, etPasswordConfirm);
 
 
     }
-    private void setImage(String url, ImageView imageView){
+
+    private void setupBtnSignUp(Button btnSignUp, EditText etUsername, EditText etFirstName, EditText etLastName, EditText etEmail, EditText etPersonalDescription, EditText etPassword, EditText etPasswordConfirm) {
+        btnSignUp.setOnClickListener(v -> {
+            String username = etUsername.getText().toString();
+            String firstName = etFirstName.getText().toString();
+            String lastName = etLastName.getText().toString();
+            String email = etEmail.getText().toString();
+            String personalDescription = etPersonalDescription.getText().toString();
+            String password = etPassword.getText().toString();
+            String passwordConfirm = etPasswordConfirm.getText().toString();
+            if (username.isEmpty() && firstName.isEmpty() && lastName.isEmpty() && email.isEmpty() && personalDescription.isEmpty() && password.isEmpty() && passwordConfirm.isEmpty()) {
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+            } else {
+                if (password.equals(passwordConfirm)) {
+                    signupNewUser(username, firstName, lastName, email, personalDescription, password);
+                } else {
+                    Toast.makeText(this, "Password doesn't matched", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
+
+    private void signupNewUser(String username, String firstName, String lastName, String email, String personalDescription, String password) {
+
+    }
+
+    private void setImage(String url, ImageView imageView) {
         int radiusIP = 100;
         Glide.with(this).load(url)
                 .apply(new RequestOptions()

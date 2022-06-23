e0d4fd1c27af08e96fb5aea1b9dd5caca8c7e4d9
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-23 15:33:37 -0700

    Setup the fragments in the main activity

--- app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
@@ -1,8 +1,14 @@
 package com.elmeradrianv.shesafe;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+//        // define your fragments here
+//        final Fragment fragment1 = new FirstFragment();
+//        final Fragment fragment2 = new SecondFragment();
+//        final Fragment fragment3 = new ThirdFragment();
+
+        setupBottomNavigationView(bottomNavigationView, fragmentManager);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        // do something here
+                        return true;
+                    case R.id.action_map:
+                        // do something here
+                        return true;
+                    case R.id.action_table:
+                        // do something here
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
     }
+
+    private void setupBottomNavigationView(BottomNavigationView bottomNavigationView, FragmentManager fragmentManager) {
+        Fragment fragment;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_profile:
+                    // do something here
+                    return true;
+                case R.id.action_map:
+                    // do something here
+                    return true;
+                case R.id.action_table:
+                    // do something here
+                    return true;
+                default:
+                    return true;
+            }
+//            fragmentManager.beginTransaction().replace(R.id.rlContainer, fragment).commit();
+//            return true;
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_map);
+    }
+
+
 }

93a6038af79f17f50566bc3ea3ec4f4ff4f242bb
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-23 15:27:14 -0700

    Setup the navigation bar

--- app/src/main/java/com/elmeradrianv/shesafe/SignUpActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/SignUpActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;

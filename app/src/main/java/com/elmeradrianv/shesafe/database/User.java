66616cc9000fad00b8a146788a6fe6b040c9796e
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-23 12:23:07 -0700

    delete unuseful methods

--- app/src/main/java/com/elmeradrianv/shesafe/database/User.java
+++ app/src/main/java/com/elmeradrianv/shesafe/database/User.java
@@ -19,49 +19,4 @@ public class User extends ParseUser {
     public static final String LAST_NAME_KEY = "lastName";
     public static final String TAG = User.class.getSimpleName();
 
-    public static void saveWithoutImage(Context context, String username, String firstName, String lastName, String email, String personalDescription, String password) {
-        ParseUser user = new ParseUser();
-        user.setUsername(username);
-        user.put(User.FIRST_NAME_KEY, firstName);
-        user.put(User.LAST_NAME_KEY, lastName);
-        user.setEmail(email);
-        user.put(User.PERSONAL_DESCRIPTION_KEY, personalDescription);
-        user.setPassword(password);
-        user.signUpInBackground(e -> {
-            if (e != null) {
-                Toast.makeText(context, "Couldn't sign up", Toast.LENGTH_SHORT).show();
-                Log.e(TAG, "signupNewUser: Signup error", e);
-                return;
-            }
-            Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show();
-        });
-    }
-
-    public static void saveWithImage(Context context, File profilePhoto, String username, String firstName, String lastName, String email, String personalDescription, String password) {
-        ParseUser user = new ParseUser();
-        ParseFile file = new ParseFile(profilePhoto);
-        file.saveInBackground((SaveCallback) e -> {
-            // If successful add file to user and signUpInBackground
-            if (e != null)
-                Log.e(TAG, "done save profile image: ", e);
-            else {
-                user.put(User.PROFILE_PHOTO_KEY, file);
-                user.setUsername(username);
-                user.put(User.FIRST_NAME_KEY, firstName);
-                user.put(User.LAST_NAME_KEY, lastName);
-                user.setEmail(email);
-                user.put(User.PERSONAL_DESCRIPTION_KEY, personalDescription);
-                user.setPassword(password);
-                user.signUpInBackground(e2 -> {
-                    if (e != null) {
-                        Toast.makeText(context, "Couldn't sign up", Toast.LENGTH_SHORT).show();
-                        Log.e(TAG, "signupNewUser: Signup error", e2);
-                        return;
-                    }
-                    Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show();
-                });
-            }
-        });
-    }
-
 }

f8b5aa050500e9eecd48cc1a8e657935ca16186c
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-28 10:02:14 -0700

    Add the table fragment

--- app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
@@ -9,9 +9,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.elmeradrianv.shesafe.fragments.MapFragment;
+import com.elmeradrianv.shesafe.fragments.TableViewFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,20 +24,20 @@ public class MainActivity extends AppCompatActivity {
         final FragmentManager fragmentManager = getSupportFragmentManager();
         // define your fragments here
         final Fragment mapFragment = new MapFragment();
-//        final Fragment fragment2 = new SecondFragment();
+        final Fragment tableFragment = new TableViewFragment();
 //        final Fragment fragment3 = new ThirdFragment();
 
-        setupBottomNavigationView(bottomNavigationView, fragmentManager,mapFragment);
+        setupBottomNavigationView(bottomNavigationView, fragmentManager, mapFragment,tableFragment);
 
     }
 
-    private void setupBottomNavigationView(BottomNavigationView bottomNavigationView, FragmentManager fragmentManager, Fragment mapFragment) {
+    private void setupBottomNavigationView(BottomNavigationView bottomNavigationView, FragmentManager fragmentManager, Fragment mapFragment, Fragment tableFragment) {
         // handle navigation selection
         bottomNavigationView.setOnItemSelectedListener(
                 new BottomNavigationView.OnItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        Fragment fragment=null;
+                        Fragment fragment = null;
                         switch (item.getItemId()) {
                             case R.id.action_map:
                                 fragment = mapFragment;
@@ -47,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
                                 //fragment = fragment2;
                                 break;
                             case R.id.action_table:
+                                fragment = tableFragment;
                             default:
-                               // fragment = fragment3;
                                 break;
                         }
                         fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();

b75246727f80ca1316f8babfaa395b7987434382
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-07-06 11:37:06 -0700

    Revert "Add ask for permission at the start of application"
    This reverts commit b23523f945ace5c3fd2f3d178d08abc61c90f6e7.

--- app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
@@ -1,29 +1,16 @@
 package com.elmeradrianv.shesafe;
 
-import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
-
-import android.Manifest;
-import android.content.Intent;
-import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.util.Log;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import com.elmeradrianv.shesafe.database.User;
 import com.elmeradrianv.shesafe.fragments.MapFragment;
 import com.elmeradrianv.shesafe.fragments.ProfileUserFragment;
 import com.elmeradrianv.shesafe.fragments.TableViewFragment;
-import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.ParseUser;
-
-import permissions.dispatcher.NeedsPermission;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,28 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setupBottomNavigationView();
         ((ImageButton)findViewById(R.id.btnSos)).setOnClickListener(v->setupSOSButton());
     }
-    @SuppressWarnings({"MissingPermission"})
-    @NeedsPermission({android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+
     private void setupSOSButton() {
-
-        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
-        locationClient.getLastLocation().addOnSuccessListener(location -> {
-                    if (location != null) {
-//                        Intent intent = new Intent(Intent.ACTION_SENDTO);
-//                        intent.setData(Uri.parse("smsto:8681585256"));  // This ensures only SMS apps respond
-//                        //https://www.google.com/maps/search/
-//                        intent.putExtra("sms_body", ParseUser.getCurrentUser().get(User.EMERGENCY_MESSAGE_KEY).toString()+" I'm in https://google.com/maps?q="+37+","+-122);
-                        SmsManager smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage("8681585256",null,ParseUser.getCurrentUser().get(User.EMERGENCY_MESSAGE_KEY).toString()+" I'm in https://google.com/maps?q="+37+","+-122,null,null);
-
-                       // intent.putExtra("sms_body", ParseUser.getCurrentUser().get(User.EMERGENCY_MESSAGE_KEY).toString()+" I'm in https://google.com/maps?q="+location.getLatitude()+","+location.getLongitude());
-                        //startActivity(intent);
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    Log.d("SOS button", "Error trying to get last GPS location");
-                    e.printStackTrace();
-                });
 
     }
 

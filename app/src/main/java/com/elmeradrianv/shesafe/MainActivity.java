aa8da604d45101779fa04b8b95b0284df902c550
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-07-06 11:57:30 -0700

    Send the emergency message to all the emergency contacts

--- app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
+++ app/src/main/java/com/elmeradrianv/shesafe/MainActivity.java
@@ -1,29 +1,78 @@
 package com.elmeradrianv.shesafe;
 
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
+import com.elmeradrianv.shesafe.database.EmergencyContacts;
+import com.elmeradrianv.shesafe.database.User;
 import com.elmeradrianv.shesafe.fragments.MapFragment;
 import com.elmeradrianv.shesafe.fragments.ProfileUserFragment;
 import com.elmeradrianv.shesafe.fragments.TableViewFragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import permissions.dispatcher.NeedsPermission;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupBottomNavigationView();
-        ((ImageButton)findViewById(R.id.btnSos)).setOnClickListener(v->setupSOSButton());
+        ((ImageButton) findViewById(R.id.btnSos)).setOnClickListener(v -> setupSOSButton());
     }
 
+    @SuppressWarnings({"MissingPermission"})
+    @NeedsPermission({android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
     private void setupSOSButton() {
 
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+        locationClient.getLastLocation().addOnSuccessListener(location -> {
+                    if (location != null) {
+                        sendEmergencyMessages(location);
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.d("SOS button", "Error trying to get last GPS location");
+                    e.printStackTrace();
+                });
+
+
+    }
+    public void sendEmergencyMessages(Location location) {
+        ParseQuery<EmergencyContacts> query = ParseQuery.getQuery(EmergencyContacts.class);
+        query.include(EmergencyContacts.USER_KEY);
+        query.whereEqualTo("user", ParseUser.getCurrentUser());
+        query.addDescendingOrder("createdAt");
+        query.findInBackground((contactsList, e) -> {
+            if (e != null) {
+                Log.e(TAG, "Issue with sending messages", e);
+                return;
+            }
+            SmsManager smsManager = SmsManager.getDefault();
+            String message = ParseUser.getCurrentUser().get(User.EMERGENCY_MESSAGE_KEY).toString() +
+                    " I'm in https://google.com/maps?q="+location.getLatitude()+","+location.getLongitude();
+            for(EmergencyContacts contact: contactsList){
+                smsManager.sendTextMessage(contact.getNumber().toString(),
+                        null,
+                        message,
+                        null, null);
+            }
+
+        });
     }
 
     private void setupBottomNavigationView() {
